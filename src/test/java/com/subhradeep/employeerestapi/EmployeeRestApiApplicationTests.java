5ef3c1988168391b9dee8cf95a8bae9f1a67f9de
Subhradeep Ray (Nokia) <subhradeepray2017@gmail.com>
2023-01-01 20:19:15 +0530

    Added / and employees endpoints

---
+++ src/test/java/com/subhradeep/employeerestapi/EmployeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhradeep.employeerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
